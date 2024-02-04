dcbc56daf20e329210704235829cf3b9244f59f3
linah <linahmuhonjaimbari>
2024-02-04 09:34:13 +0300

    add  useNavigate component

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
 import './Styling/Home.css'
 
 function Home() {
+    const navigate = useNavigate()
+    return (
+        <div className='body_content'>
+            <div className='hero_content'>
+                <div className='hero_content_right'>
+                    <h1>Tanui Industries Inventory </h1>
+                    <p>Add on the products</p>
+                </div>
+                <div>
+                    <button className='expired_contracts' onClick={() => navigate('/create')}>Add Products</button>
+                </div>
+            </div>
+            
+            <div className='footer_copyright'>
+                <small>&copy;Tanui Industries. All rights reserved.</small>
+            </div>
+        </div>
+    )
   return (
     <div className='body_content'>
         

70e919bd54ce320594c495557dcb1cbf7d125899
linah <linahmuhonjaimbari>
2024-02-04 12:02:45 +0300

    add fetch api for products

--- src/App.js
+++ src/App.js
@@ -6,9 +6,23 @@ import { MdOutlineFactory } from "react-icons/md";
 import  Create from "./Create";
 import Products from "./Products";
 import './Styling/Home.css'
+import { useEffect, useState } from 'react';
+import ProductContainer from './ProductContainer';
 
 function App() {
   const navigate = useNavigate()
+  const [records, setRecords] = useState([]);
+
+  useEffect(()=>{
+    fetch('./products')
+    .then(resp => resp.json())
+    .then(setRecords)
+  }, [])
+
+  const addProductHandler = (newProduct) =>{
+    const add = {...records, newProduct}
+    setRecords(add);
+  }
   return (
     <div className="App">
        <div className='navbar_container'>
@@ -29,8 +43,9 @@ function App() {
         </div>
       <Routes>
         <Route path  ="/" element ={<Home />} />
-        <Route path  = "/products"  element ={<Products />} />
-        <Route path  = "/create"  element ={< Create/>} />
+        <Route  path = "/products" element ={<Products passProduct = {records} />}/>
+        {/* <Route path  = "/product-container"  element ={<ProductContainer passProducts = {records}/>} /> */}
+        <Route path  = "/create"  element ={< Create addProducts = {addProductHandler}/>} />
       </Routes>
 
       <div className='footer_copyright'>

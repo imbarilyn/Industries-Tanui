fc83354eb16600ad80838b1616cf041ded091fd6
linah <linahmuhonjaimbari>
2024-02-04 12:02:18 +0300

    add a table for data display

--- src/Products.js
+++ src/Products.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import {Link } from 'react-router-dom'
 
-function Products() {
+function Products({product}) {
   return (
-    <div>
-        <p>Hey</p>
-    </div>
+    <div className='main-content'>
+    <div className='contracts-top'style={{float: "right"}}>
+      <div className='search-tab'style={{ marginRight: "10px", marginTop: "4px"}}>
+        <label>search  </label><input type='search' style={{border:"1px solid black", height:"30px"}}/>
+      </div>
+      <br></br>
+      <div className='add-contract'>
+        <Link to = "/create"><button type="button" className="btn btn-success" style={{float: "right", marginRight: "3px", marginTop: "0px", backgroundImage: "linear-gradient(79deg, #3277a8, #3283a8)" }}>Add+</button></Link>
+      </div>
+  </div>
+    
+  <div className='Contracts-display'>
+        <table className="table table-hover">
+          <thead>
+            <tr>                
+              <th>No.</th>
+              <th>Product Name</th>   
+              <th>Product Description</th>
+              <th>Price</th>  
+              <th>Quantity</th>                        
+            </tr>
+          </thead>
+          <tbody> 
+            {
+              product.map((i, j) =>(
+                <tr>             
+                <td>{i.id}</td>
+                <td>{i.name}</td>
+                <td>{i.description}</td>
+                <td>{i.price}</td> 
+                <td>{i.quantity}</td>
+                <td>
+                  <button  type="button" class="btn btn-outline-primary" style={{margin: "4px"}}>Update</button>
+                  <button type="button" class="btn btn-outline-danger">Delete</button>
+                </td>
+              </tr>
+
+              )
+              )
+             
+            }
+         
+            
+          </tbody>
+        </table>
+  </div>     
+</div>
   )
 }
 
